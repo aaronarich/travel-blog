42416eed76a16b5e6fb68962995d9ba260a3f319
Alex Miles <howdy@alexmilesdesign.com>
2013-06-23 17:01:52 +0200

    Add hover to zoom function

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -15,9 +15,6 @@ $(function() {
     return time;
   }
 
-  // Hover over map to change image
-
-
   // The initial request URL
   var reqURL = "https://api.instagram.com/v1/users/14989250/media/recent/?access_token=14989250.a1982d7.a9539f2ed5744019b8bcf46a876152df";
 
@@ -30,8 +27,6 @@ $(function() {
         url: reqURL,
         success: function(data) {
 
-          //console.log(data);
-
           // Get the amount of photos in the object
           var count = data.data.length;
 
@@ -39,9 +34,18 @@ $(function() {
           for (var i = 0; i < count; i++) {
 
             // Google static map api
-            var map = 'http://maps.googleapis.com/maps/api/staticmap?' +
+            var mapClose = 'http://maps.googleapis.com/maps/api/staticmap?' +
                       'center=' + data.data[i].location.latitude + ',' + data.data[i].location.longitude + '&' +
-                      'zoom=13&' +
+                      'zoom=12&' +
+                      'size=612x100&' +
+                      // Cant get custom icon to work. Fix this first
+                      'markers=icon:http://i.imgur.com/QbSul.png%7Cshadow:false%7C' + data.data[i].location.latitude + ',' + data.data[i].location.longitude + '&' +
+                      'sensor=true';
+
+            // Google static map api
+            var mapFar = 'http://maps.googleapis.com/maps/api/staticmap?' +
+                      'center=' + data.data[i].location.latitude + ',' + data.data[i].location.longitude + '&' +
+                      'zoom=4&' +
                       'size=612x100&' +
                       // Cant get custom icon to work. Fix this first
                       'markers=icon:http://i.imgur.com/QbSul.png%7Cshadow:false%7C' + data.data[i].location.latitude + ',' + data.data[i].location.longitude + '&' +
@@ -82,7 +86,7 @@ $(function() {
                   "</a>" +
                 "</div>" +
                 "<div class='meta'>" +
-                  "<img class='face-map' src='" + map + "' />" +
+                  "<img class='face-map' data-map-far='" + mapFar + "' data-map-close='" + mapClose + "' src='" + mapFar + "' />" +
                   "<div class='info'>" +
                     caption + locationName + " &nbsp;" + datePosted +
                   "</div>" +
@@ -93,29 +97,41 @@ $(function() {
 
           // Get url to load more photos
           window.nextPage = data.pagination.next_url;
-
           $('.load-more').html('Load More');
-
           if ($('li').length > 0){
             $('.first-load').hide();
           }
-
         }
     });
+
+    // Run function to show detailed map on hover
+    showMapDetail();
+  }
+
+  function showMapDetail() {
+    $(document).on({
+      mouseenter: function() {
+        var mapCloseSrc = $(this).data('map-close');
+        var mapFarSrc = $(this).data('map-far');
+        $(this).attr('src', mapCloseSrc);
+      },
+      mouseleave: function(){
+        var mapCloseSrc = $(this).data('map-close');
+        var mapFarSrc = $(this).data('map-far');
+        $(this).attr('src', mapFarSrc);
+      }
+    }, '.face-map');
   }
 
   // Load most recent photos
   loadInsta(reqURL);
-
 
   // Load more button
   $('.load-more').click(function(){
     console.log('next page: ' + nextPage);
     $(this).html('<img src="images/ajax-loader.gif" />');
     loadInsta(nextPage);
-
     return false;
-
   });
 
   // Scroll back to top button
@@ -124,9 +140,7 @@ $(function() {
     return false;
   });
 
-
   $(window).scroll(function () {
-
     // Show scroll top button after user starts to scroll
     var fromTop = $(window).scrollTop();
     if (fromTop > 1000) {
@@ -134,6 +148,5 @@ $(function() {
     } else {
       $(".scroll-top").fadeOut(300);
     }
-
   });
 });
